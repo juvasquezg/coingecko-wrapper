c1c72dc2b91d4d16dd845c31a5cab68e8e631cad
julianvasquez <julian.vasquez@netuxtecnologia.com>
2021-01-06 23:22:14 -0500

    feat(migrations): add cryptocurrencies table

--- src/models/users.js
+++ src/models/users.js
@@ -9,8 +9,11 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate (_models) {
-      // define association here
+    static associate (models) {
+      User.hasMany(models.Cryptocurrency, {
+        foreignKey: 'user_id',
+        as: 'cryptocurrencies'
+      })
     }
   }
   User.init(
